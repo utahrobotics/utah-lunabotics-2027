d4a42a76b6e1c07376abd95ef390b54e8b4e2271
jaycoding-hub <julianthomasedu@gmail.com>
2026-09-23 18:51:36 -0600

    Updated control config

--- godot/new-lunabase/Systems/ControlSchemes/control_config_loader.gd
+++ godot/new-lunabase/Systems/ControlSchemes/control_config_loader.gd
@@ -1,9 +1,18 @@
 class_name ControlConfigLoader
 extends RefCounted
 
-const USER_CONFIG_PATH := "user://control_config.json"
+const USER_CONFIG_PATH := "user://control_config.mock.json"
 const EXTERNAL_CONFIG_FILENAME := "control_config.json"
 const VERSION := 1
+
+const GAMEPAD_BUTTONS := {
+	"options": 6,
+	"left_stick_click": 7,
+	"l1": 9,
+	"r1": 10,
+	"d_pad_up": 11,
+}
+
 const FLAGS_KEY := "flags"
 const DEFAULT_FLAGS := {
 	"apply_steering_axis_deadzone": true,
@@ -185,9 +194,12 @@ static func _token_to_event(token: String) -> InputEvent:
 			key_event2.keycode = guess
 			return key_event2
 	if token.begins_with("gamepad_button_"):
-		var b := InputEventJoypadButton.new()
-		b.button_index = int(token.trim_prefix("gamepad_button_"))
-		return b
+		var button_name = token.trim_prefix("gamepad_button_")
+		
+		if GAMEPAD_BUTTONS.has(button_name):
+			var b := InputEventJoypadButton.new()
+			b.button_index = GAMEPAD_BUTTONS[button_name]
+			return b
 	if token.begins_with("gamepad_axis_"):
 		var tail := token.trim_prefix("gamepad_axis_")
 		var bits := tail.split("_")
